04fbccc55c4febb4f34b17b3c833f0e66a42e828
SpurthiM16 <122963690+SpurthiM16@users.noreply.github.com>
2023-02-06 19:15:56 +0530

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install 15.0.0
+echo "Confirm Successful Installation"
+
+node -e "console.log('Running Node.js ' + process.version)"
+
 echo "Welcome to Amantya Technologies!" > index.html
 
 npm install -g http-server --prefix ~/npm
 http-server -p 8055 &
-
-
