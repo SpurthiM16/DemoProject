fbbe565bb6ba9758c1197dc01e7ea26f2803425d
SpurthiM16 <122963690+SpurthiM16@users.noreply.github.com>
2023-02-06 19:19:53 +0530

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_19.x | sudo -E bash - 
+sudo yum install -y nodejs 
 
-. ~/.nvm/nvm.sh
-
-nvm install 15.0.0
 echo "Confirm Successful Installation"
-
-node -e "console.log('Running Node.js ' + process.version)"
 
 echo "Welcome to Amantya Technologies!" > index.html
 
